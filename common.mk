3875aba01ee41221e1d9c21e181cafb402e55573
spezi77 <spezi7713@gmx.net>
2019-04-16 21:03:00 +0200

    Remove DU default wallpaper & bootanimation. - Revert "Add new default wallpaper" This reverts commit 7d4c88bdd1936186a3f70296ed837993195f29ed. - Remove du bootanimation to save space.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- common.mk
+++ common.mk
@@ -4,9 +4,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 # Accents
 #include vendor/themes/accents.mk
 
-# Boot animation
-include vendor/themes/bootanimation.mk
-
 # Sounds
 #include vendor/themes/sounds.mk
 
