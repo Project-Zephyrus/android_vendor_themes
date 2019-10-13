01636efe0bef3da32c1ceeabd2964886f1f878f0
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-13 16:31:39 +0530

    Add our favourite fonts in AOSP FontEngine [1/2]
    spezi77: Squash in additonally required fonts for Xtended Statusbar Clock Customization.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- common.mk
+++ common.mk
@@ -4,6 +4,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 # Accents
 #include vendor/themes/accents.mk
 
+# Fonts
+include vendor/themes/fonts.mk
+
 # Sounds
 include vendor/themes/sounds.mk
 
