7aa960f22c9a4fb73f17d8e946ace49c2f96b1b2
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-06-30 16:40:38 +0200

    themes: bunch of random fixes
    * fix RROs
    * don't set default font here
    * revert back to AOSP back gesture icon
    * values.xml -> styles.xml in primary overlays
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -1,9 +1,6 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
-# Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
-
 # Accents
 include vendor/themes/accents.mk
 
