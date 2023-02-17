1ea1ccda997ea8a0505ea200642bbf08a52e00cf
Ziauddin Sameer <ziauddinsameer@gmail.com>
2023-02-17 23:31:42 +0000

    themes: Stop building custom overlays for now

--- iconpacks.mk
+++ iconpacks.mk
@@ -1,29 +1,29 @@
 LOCAL_PATH := vendor/themes
 
 # Statusbar Icons
-PRODUCT_PACKAGES += \
-    StrokeSignalOverlay \
-    SneakySignalOverlay \
-    XperiaSignalOverlay \
-    ZigZagSignalOverlay \
-    WavySignalOverlay \
-    RoundSignalOverlay \
-    InsideSignalOverlay \
-    DoraSignalOverlay \
-    BarsSignalOverlay
+#PRODUCT_PACKAGES += \
+#    StrokeSignalOverlay \
+#    SneakySignalOverlay \
+#    XperiaSignalOverlay \
+#    ZigZagSignalOverlay \
+#    WavySignalOverlay \
+#    RoundSignalOverlay \
+#    InsideSignalOverlay \
+#    DoraSignalOverlay \
+#    BarsSignalOverlay
 
 # Wi-Fi Icons
-PRODUCT_PACKAGES += \
-    StrokeWiFiOverlay \
-    SneakyWiFiOverlay \
-    XperiaWiFiOverlay \
-    ZigZagWiFiOverlay \
-    WavyWiFiOverlay \
-    RoundWiFiOverlay \
-    InsideWiFiOverlay \
-    WeedWiFiOverlay \
-    DoraWiFiOverlay \
-    BarsWiFiOverlay
+#PRODUCT_PACKAGES += \
+#    StrokeWiFiOverlay \
+#    SneakyWiFiOverlay \
+#    XperiaWiFiOverlay \
+#    ZigZagWiFiOverlay \
+#    WavyWiFiOverlay \
+#    RoundWiFiOverlay \
+#    InsideWiFiOverlay \
+#    WeedWiFiOverlay \
+#    DoraWiFiOverlay \
+#    BarsWiFiOverlay
 
 # ThemesIcons
 PRODUCT_PACKAGES += \
