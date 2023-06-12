40e2ed04ec871ea465d5324eab9ad9e5b846a10e
Ziauddin Sameer <ziauddinsameer@gmail.com>
2023-06-12 11:58:35 +0530

    themes: added clock fonts and rebamped fonts section (Taken From risingOS) All credit goes to the devs who created these fonts

--- fonts.mk
+++ fonts.mk
@@ -2,42 +2,86 @@ LOCAL_PATH := vendor/themes
 
 # Fonts
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
+# Fonts
 PRODUCT_PACKAGES += \
-    FontInterOverlay \
-    FontLinotteSourceOverlay \
-    FontManropeOverlay \
-    FontOnePlusSansOverlay \
-    FontOneplusSlateSourceOverlay \
-    FontAppleMintOverlay \
-    FontCartoonistHandOverlay \
-    FontChinacatOverlay \
-    FontLatoOverlay \
-    FontRosemaryOverlay \
-    FontSanFransiscoOverlay \
-    FontSlateProOverlay \
-    FontZillaSlabOverlay \
-    FontCoreMellow \
-    FontFucek \
-    FontLemon \
-    FontSamsungSans \
-    FontSimpleDay \
-    FontUbuntu \
-    FontBariolSourceOverlay \
-    FontAclonicaSourceOverlay \
-    FontAmaranteSourceOverlay \
-    FontCagliostroSourceOverlay \
-    FontComfortaaSourceOverlay \
-    FontComicSansSourceOverlay \
-    FontCoolstorySourceOverlay \
-    FontLGSmartGothicSourceOverlay \
-    FontSamsungOneSourceOverlay \
-    FontSonySketchSourceOverlay \
-    FontSurferSourceOverlay \
-    FontStoropiaOverlay \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontBariolOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvolveSansOverlay \
+    FontFluidSansOverlay \
+    FontGoogleSansOverlay \
+    FontHarmonySansOverlay \
+    FontJTLeonorOverlay \
+    FontLetteraOverlay \
+    FontLinotteOverlay \
+    FontMiSansOverlay \
+    FontNokiaPureOverlay \
     FontNothiingDotOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
     FontOppoSansOverlay \
     FontProductSansVHOverlay \
-    FontNothingDotHeadlineOverlay
+    FontRobotoCondensedOverlay \
+    FontRosemaryOverlay \
+    FontRubikOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSamsungOneOverlay \
+    FontSimpleDaySourceOverlay \
+    FontSonySketchOverlay
+
+# Lockscreen clock fonts
+PRODUCT_PACKAGES += \
+    ClockFontAlienLeagueOverlay \
+    ClockFontBalticBoddenOverlay \
+    ClockFontBalticStormOverlay \
+    ClockFontBigNoodleTiltingOverlay \
+    ClockFontMXWasgardOverlay \
+    ClockFontProdeltCoOverlay \
+    ClockFontRoadRageOverlay \
+    ClockFontTourneyMediumOverlay \
+    ClockFontZeroFourOverlay \
+    ClockFontCatOverlay \
+    ClockFontConcentrateOverlay \
+    ClockFontKarmaticArcadeOverlay \
+    ClockFontLiquidCrystalOverlay \
+    ClockFontVG5000Overlay \
+    ClockFontAccuratistOverlay \
+    ClockFontApiceOverlay \
+    ClockFontAudimatOverlay \
+    ClockFontBariolOverlay \
+    ClockFontCoconOverlay \
+    ClockFontComfortaaOverlay \
+    ClockFontComicSansOverlay \
+    ClockFontCoolstoryOverlay \
+    ClockFontExotwoOverlay \
+    ClockFontEvolveSansOverlay \
+    ClockFontFluidSansOverlay \
+    ClockFontGoogleSansOverlay \
+    ClockFontGeometosOverlay \
+    ClockFontHarmonySansOverlay \
+    ClockFontJTLeonorOverlay \
+    ClockFontLetteraOverlay \
+    ClockFontLinotteOverlay \
+    ClockFontMiSansOverlay \
+    ClockFontNokiaPureOverlay \
+    ClockFontNothingDotHeadlineOverlay \
+    ClockFontOneplusSansOverlay \
+    ClockFontOneplusSlateOverlay \
+    ClockFontOppoSansOverlay \
+    ClockFontProductSansVHOverlay \
+    ClockFontRobotoCondensedOverlay \
+    ClockFontRosemaryOverlay \
+    ClockFontRubikOverlay \
+    ClockFontSanFranciscoDisplayProSourceOverlay \
+    ClockFontSamsungOneOverlay \
+    ClockFontSimpleDaySourceOverlay \
+    ClockFontSonySketchOverlay
